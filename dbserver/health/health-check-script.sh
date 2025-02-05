3b62dd3e903ace4ef2e53d7a073c74e1c2e11bda
VishnuPrashanthDev <vishnu7stanite@gmail.com>
2025-02-05 13:25:48 +0000

    check

--- dbserver/health/health-check-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the database server is reachable
-if nc -z -w 1 db.cyber23test 3306; then
-    exit 0  # Success
-else
-    exit 1  # Failure
-fi
--- webserver/configfiles/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p /run/php-fpm/
-/usr/sbin/nginx
-/usr/sbin/php-fpm -F
-/usr/sbin/sshd -D
